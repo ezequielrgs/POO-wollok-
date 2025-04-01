cce9b1265ef3729673a9b97678dd6ff60f7dfbb4
Claudix <claudio.sosa.e21.21@gmail.com>
2025-04-01 10:24:49 -0300

    me cago en la puta, no se subió lo del codespace y lo tengo que hacer de nuevo. AYUDA :(

--- Batalla_naval_python/BatallaNavalBorradorAnterior.py
+++ Batalla_naval_python/BatallaNavalBorradorAnterior.py
@@ -1,8 +1,9 @@
+from random import randint
 BARCOS = {
-    1: [(0, 0)],  # Barco de tamaño 1
-    2: [(0, 0), (0, 1)],  # Barco de tamaño 2 con rotación horizontal
-    3: [(0, 0), (0, 1), (0, 2)],  # Barco de tamaño 3 con rotación horizontal
-    4: [(0, 0), (0, 1), (0, 2), (0, 3)]  # Barco de tamaño 4 con rotación horizontal
+    1: [[(0, 0)]],  # Barco de tamaño 1
+    2: [ [(0, 0), (0, 1)], [(0,0), (1,0)] ],  # Barco de tamaño 2 con rotación horizontal
+    3: [[(0, 0), (0, 1), (0, 2)], [(0,0),(1,0),(2,0),(3,0)]],  # Barco de tamaño 3 con rotación horizontal
+    4:[ [(0, 0), (0, 1), (0, 2), (0, 3)], [(0, 0), (1, 0), (2, 0), (3, 0)]]  # Barco de tamaño 4 con rotación horizontal
 }
 
 def dimensiones(x: int, y: int) -> tuple:
@@ -61,7 +62,7 @@ def cantidad_objetos(c=1) -> int:
 def celda_vacia(coord: tuple, boat: list, grid: list) -> bool:
     y, x = coord
     # Se verifica que la celda esté vacía (0) y opcionalmente que no forme parte del barco (boat)
-    if grid[y][x] != 0 and coord not in boat:  #<-- Si se quiere tener en cuenta lo de boat, quitar el comentario
+    if grid[y][x] != 0 and coord not in boat:
         return False
     return True
 
@@ -72,8 +73,7 @@ def graficar(grilla: list) -> None:
 
 
 def generar_coords(grid: list) -> tuple:
-    from random import randint
-    return (randint(0, len(grid[0]) - 1), (randint(0, len(grid) - 1)))
+    return (randint(0, len(grid[0]) - 1), randint(0, len(grid) - 1))
 
 
 def put_in_grill_process(coords: tuple, grid: list, boat = []) -> list:
@@ -99,43 +99,11 @@ def disparo(coord_apuntada:tuple, grid:list)->list:
         grid[y][x] = 2
         return [grid, is_a_boat] #Este 'return' indica que se ACERTÓ el dísparo.(True)
     return [grid, is_a_boat] #Este otro 'return' indica que NO se acertó.(False)
-
-
-def intelligence(coords:tuple, grid:list, UsedCoords = [])->list:
-    if not UsedCoords: 
-        #
-        # **Si UsedCoords no tiene coordenadas guardadas, 
-        # **el programa disparará automaticamente
-        # #
-        UsedCoords.append(coords)
-        return disparo(coord_apuntada=coords, grid=grid)
     
-    if disparo(coord_apuntada=coords, grid=grid)[1]:
-        #
-        # El barco tiene qe hundirse.#
-        grid[coords[1]][coords[0]] = 2
-        return grid
-
-    #
-    # El barco no debe hundirse.#    
-    new_grid = grid[:]
-    
-    if not coords in UsedCoords:
-        UsedCoords.append(coords)
-        new_grid, collapse = disparo(coord_apuntada=coords, grid=new_grid)
-        if collapse[1] is True:
-            new_grid = collapse[0]
-            return [new_grid, True]
-    elif coords in UsedCoords:
-        pass
-    else:
-        raise "Error"
-    
-def aiming(grid:list)->tuple:
-    from random import randint
-    return (randint(0, len(grid) -1), randint(0, len(grid[0]) - 1))
 
 def shooting_process(grid: list, aiming_coords: tuple, used_coords : list)->None:
+            if aiming_coords in used_coords:
+                raise ValueError
             res_shoot = disparo(coord_apuntada=aiming_coords, grid= grid)
             #
             #   ***********************************************************
@@ -147,9 +115,9 @@ def shooting_process(grid: list, aiming_coords: tuple, used_coords : list)->None
             #   *                     |_>Segunda posibilidad: Barco(True) *    
             #   ***********************************************************
             ##
-            if res_shoot is False: 
+            if res_shoot[1] is False: 
               used_coords[0].append(aiming_coords)
-            elif res_shoot is True:
+            elif res_shoot[1] is True:
                 used_coords[1].append(aiming_coords)
 
 def breaking_the_habit(types_boats_count: dict, breaker = False)-> bool:
@@ -163,10 +131,8 @@ def breaking_the_habit(types_boats_count: dict, breaker = False)-> bool:
     return breaker
 
 def get_coords_based(grid: tuple, bsed_coords: list)-> tuple:
-    from random import randint
     boats = bsed_coords[1]
     water = bsed_coords[0]
-
 
     branch_of_BsedCoords = randint(0, 1)
     match branch_of_BsedCoords:
@@ -182,13 +148,11 @@ def get_coords_based(grid: tuple, bsed_coords: list)-> tuple:
                     for x in range(lx, min(lenx, bx+1)):
                         if (x == lx and y == ly) or (x == min(len(lenx, bx+1)) and y == min(len(lenx, by+1))):
                             continue
-
-                        #if 
-
+                        elif grid[y][x] == 2 or (y,x) == boats_kicked:
+                            continue
+                        if grid[y][x] == 1:
+                            return y,x
                         
-                        
-            
-    
         case _:
             for water_cell in water:
                 lenx = len(grid[0])
@@ -201,7 +165,6 @@ def get_coords_based(grid: tuple, bsed_coords: list)-> tuple:
 
             pass
 def main() -> None:
-    from random import randint
     #objs = input("Cuantos objetos quiere ingresar: ")
    # cantidad = cantidad_objetos(c=int(objs))
     ix = input("X = ")
@@ -217,11 +180,11 @@ def main() -> None:
 
     for _ in range(len(numbers_of_types_boats)):
         key:int = randint(1, len(BARCOS))
-        type_of_boat = BARCOS.get(key)
+        type_of_boat = BARCOS.get(key)[randint(0, len(BARCOS[key])-1)]
         all_placed = False
 
         while not all_placed:
-            coords = aiming(grid)
+            coords = generar_coords(grid)
             new_grid = put_in_grill_process(coords, grid, type_of_boat)
             if new_grid != grid:  # Si la grilla ha cambiado, el barco se colocó
                 grid = new_grid
@@ -238,10 +201,12 @@ def main() -> None:
             print(attemps)
             break
         else:
-
-            coords = (len(used_coords[0]) == 0) and (len(used_coords[1]) == 0) if aiming(grid) else get_coords_based(grid, used_coords)
-            shooting_process(grid=grid, aiming_coords=coords, used_coords=used_coords)
-            
+            coords =  generar_coords(grid) if (len(used_coords[0]) == 0) and (len(used_coords[1]) == 0) else get_coords_based(grid, used_coords)
+            try:
+                shooting_process(grid=grid, aiming_coords=coords, used_coords=used_coords)
+            except ValueError:
+                shooting_process(grid, get_coords_based(grid, used_coords), used_coords)
+                
     print(graficar(grid))
 
 main()
